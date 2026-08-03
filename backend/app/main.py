ed9c4d2a1009a250cb64c43588348e46181fa68f
Suhas Vankadari <suhasv910@gmail.com>
2026-08-03 11:01:54 +0530

    initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from app.tmdb_service import get_popular_movies, search_movie
+
+
+app = FastAPI()
+
+movie_posts = []
+
+
+@app.get("/")
+def hello_world():
+    return {"Message": "Hello World"}
+
+
+@app.get("/popular_movies")
+def popular_movies():
+    data = get_popular_movies()
+    return data
+
+
+@app.get("/movies/{query}")
+def search_movies(query: str):
+    data = search_movie(query)
+    if data is None:
+        raise HTTPException(status_code=404, detail="Movie not found")
+    else:
+        first_item = data["results"][0]
+        return first_item
+
+@app.post("/movies/{query}")
+def add_movie(query: str):
+    data = search_movie(query)
+    first_item = data["results"][0]
+    movie_posts.append(first_item)
+    return {"message": "Movie added successfully"}
+
+
---
+++ backend/app/tmdb_service.py
@@ -0,0 +1,28 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+
+BASE_URL = "https://api.themoviedb.org/3"
+
+def get_popular_movies():
+    """ to get popular movies"""
+    url = f"{BASE_URL}/movie/popular"
+    params = {"api_key": TMDB_API_KEY}
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data
+
+def search_movie(query):
+    url = f"{BASE_URL}/search/movie"
+    params = {"api_key": TMDB_API_KEY, "query":query}
+
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
