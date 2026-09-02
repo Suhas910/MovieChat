928684bf0ff3043a4187892ac9a0689a4c75cb59
Suhas Vankadari <suhasv910@gmail.com>
2026-09-02 22:19:33 +0530

    changed CORS

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://moviechat-api.onrender.com';
 
 async function request(path, { method = 'GET', body, token, queryParams } = {}) {
   const headers = { 'Content-Type': 'application/json' };
